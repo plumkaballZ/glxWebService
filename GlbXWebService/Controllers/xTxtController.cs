804bf3381cd8ec41abeddec6cfb294639af884c3
Ervin Ramic <ervin@flexpos.com>
2018-02-19 15:33:35 +0100

    asdf

--- GlbXWebService/Controllers/xTxtController.cs
+++ GlbXWebService/Controllers/xTxtController.cs
@@ -49,8 +49,9 @@ namespace GlbXWebService.Controllers
             }
 
             if (isNewLine) arrLines.Add(txtFile.rawStr);
+       
 
-            System.IO.File.WriteAllText("/home/plumka/website/glxWebService/GlbXWebService/txt/", string.Join("\n", arrLines));
+            System.IO.File.WriteAllText("/home/plumka/website/glxWebService/GlbXWebService/txt/" + txtFile.fileName, string.Join("\n", arrLines));
 
             //System.IO.File.WriteAllLines("/home/plumka/website/glxWebService/GlbXWebService/txt/" + txtFile.fileName, arrLines);
 

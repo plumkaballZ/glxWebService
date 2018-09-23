db7f9fb9c0e1e78b3828bd836658c07d49f2aab4
Ervin Ramic <ervin@flexpos.com>
2018-09-23 15:49:13 +0200

    added pakkelabels api

---
+++ GlbXWebService/Controllers/xPakkelabelsController.cs
@@ -0,0 +1,35 @@
+﻿using Flurl.Http;
+using GlbXWebService._logics;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GlbXWebService.Controllers
+{
+    [Route("api/[controller]")]
+    public class xPakkelabelsController : Controller
+    {
+        private PakkeLabelsApiClient _apiClinet;
+
+        public xPakkelabelsController()
+        {
+            _apiClinet = new PakkeLabelsApiClient();
+        }
+
+        [HttpGet]
+        [EnableCors("AllowAllOrigins")]
+        public string Get()
+        {
+
+            _apiClinet.test();
+
+            return "service_running_x4";
+        }
+
+    }
+}
---
+++ GlbXWebService/_logics/PakkeLabelsApiClient.cs
@@ -0,0 +1,30 @@
+﻿using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlbXWebService._logics
+{
+    public class PakkeLabelsApiClient
+    {
+        private string _urlV1 = "https://app.pakkelabels.dk/api/public/v1/";
+        private string _urlV2 = "https://app.pakkelabels.dk/api/public/v2/";
+       
+
+
+        public void test()
+        {
+            ((Action)(async () =>
+            {
+                var responseString = await "https://app.pakkelabels.dk/api/public/v2/users/login"
+                .PostUrlEncodedAsync(new { api_user = "0930aeb1-1412-4260-9f19-63fb1d010720", api_key = "1031bb73-6770-46eb-9960-6b1ab31d322e" })
+                .ReceiveString();
+
+                Debug.WriteLine(responseString);
+
+            }))();
+        }
+    }
+}
